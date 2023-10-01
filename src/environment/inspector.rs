ddaeeae763d2cf400987c29c09e2eb26642831ee
y-jiji <y.jijiji.data.science@gmail.com>
2023-10-01 10:16:15 +0800

    Add: more general interfaces for defenders and attackers.

--- src/environment/inspector.rs
+++ src/environment/inspector.rs
@@ -1,67 +1,127 @@
 use revm::interpreter::*;
 use revm::primitives::*;
-use revm::primitives::bitvec::view::AsBits;
-use super::bitwig;
-use bitvec::vec::BitVec;
-use revm::{Inspector, create_evm_impl, Database, EVMData};
+use revm::{create_evm_impl, Database, EVMData, Inspector};
 
-pub struct GameInspector {
-    // adimistrator account
-    admin: B160,
-    // set of function calls
-    target: Vec<(B160, u32)>,
-    // defender's action
-    defender: HashMap<(B160, u32), Vec<bitwig::Instruction>>,
-    // defender state stack
-    dfdstate: Vec<Vec<bool>>,
-    // attacker address, code for calls and code for returns
-    attacker: (B160, Vec<bitwig::Instruction>, Vec<bitwig::Instruction>),
-    // attacker state in bitlanguage
-    attstate: Vec<bool>,
-    // the rest chances of the attacker
-    attacker_rest_calls: usize,
+pub trait Defender {
+    type State;
+    // initialize a state
+    fn init(&mut self, contracts: Vec<(B160, Bytes)>) -> Self::State;
+    // check current contract
+    fn check(&self, state: &Self::State, inputs: &CallInputs) -> (Self::State, bool);
 }
 
-impl<DB: Database> Inspector<DB> for GameInspector {
-    fn call(&mut self, data: &mut EVMData<'_,DB> , inputs: &mut CallInputs) -> (InstructionResult, Gas, Bytes) {
-        let contract = inputs.contract;
-        let bytes = &inputs.input;
-        let funct: u32 = u32::from_be_bytes(bytes.get(..4)
-            .map(|x| x.try_into().expect("length don't match"))
-            .unwrap_or([0u8; 4]));
-        if let Some(wig) = self.defender.get(&(contract, funct)) {
-            let prev = self.dfdstate.last().unwrap();
-            let next = bitwig::evaluate(bytes.as_bits(), prev.clone(), wig);
-            let x = next[0]; self.dfdstate.push(next);
-            (if x { InstructionResult::Continue } else { InstructionResult::Revert }, 
-                Gas::new(0), Bytes::default())
-        } else if contract == self.attacker.0 {
-            // a state change using function 1
-            self.attstate = bitwig::evaluate(
-                &BitVec::from_element(1u8), 
-                self.attstate.clone(), 
-                &self.attacker.1
-            );
-            // struct EVMImpl is tweaked, we can execute a function call from here
+pub trait Attacker {
+    type State;
+    // initialize a state
+    fn init(&mut self, contracts: Vec<(B160, Bytes)>) -> Self::State;
+    // generate a sequence of function calls
+    fn make_call(&self, state: &mut Self::State) -> Option<CallInputs>;
+    // process a call return
+    fn take_return(&self, state: &mut Self::State, ret: InstructionResult, gas: Gas, out: Bytes);
+    // check made by attacker
+    fn check(&self, state: &mut Self::State) -> bool;
+}
+
+pub struct GameInspector<DP: Defender, AP: Attacker> {
+    // adimistrator account
+    admin: B160,
+    // the upper limit of attacker actions
+    limit: usize,
+    // set of targets, attacker account
+    accounts: (HashSet<B160>, B160),
+    // pure strategy of a defender
+    defender: DP,
+    defstate: Vec<DP::State>,
+    // pure strategy of an attacker
+    attacker: AP,
+    attstate: AP::State,
+}
+
+lazy_static::lazy_static!{
+    static ref INIT_CODE: Bytes = 
+        Bytes::from(hex::decode(
+            "6080604052348015600f57600080fd5b50603f80601d6000396000f3fe6080604052600080fdfea26469706673582212203e9cd2e4b65a21d520d56991fdd1bc3ef05a91ad81ef47da4ff48e58372c440164736f6c63430008120033"
+        ).unwrap());
+}
+
+impl<DB: Database, DP: Defender, AP: Attacker> 
+    Inspector<DB> for GameInspector<DP, AP> 
+{
+    fn call(
+        &mut self,
+        data: &mut EVMData<'_, DB>,
+        inputs: &mut CallInputs,
+    ) -> (InstructionResult, Gas, Bytes) {
+        if self.admin == inputs.contract {
+            let admin = inputs.contract;
+            // administrator action: just call the attacker
             let mut evm = create_evm_impl::<DB, true>(data, self);
-            evm.call(inputs);
-            todo!()
-        } else {
+            // call account creating
+            let Some(address) = evm.create(
+                &mut CreateInputs {
+                    caller: admin, 
+                    scheme: CreateScheme::Create, 
+                    value: U256::MAX / U256::from(2), 
+                    init_code: INIT_CODE.clone(), 
+                    gas_limit: 1 << 25,
+                }
+            ).1
+            else {
+                panic!("attacker account cannot be created. ")
+            };
+            // call attacker code
+            evm.call(&mut CallInputs {
+                // attacker account
+                contract: address, 
+                transfer: Transfer { source: admin, target: address, value: U256::ZERO },
+                input: Bytes::new(), 
+                gas_limit: 1 << 25, 
+                context: CallContext {
+                    address, 
+                    caller: admin, 
+                    code_address: address, 
+                    apparent_value: U256::ZERO, 
+                    scheme: CallScheme::Call,
+                }, 
+                is_static: false, 
+            })
+        }
+        else if self.accounts.0.contains(&inputs.contract) {
+            // check before function calls
+            let (state, ok) = self.defender.check(self.defstate.last().unwrap_or_else(|| panic!()), inputs);
+            self.defstate.push(state);
+            let ok = if ok { InstructionResult::Continue } else { InstructionResult::Revert };
+            (ok, Gas::new(0), Bytes::default())
+        }
+        else if self.accounts.1 == inputs.contract {
+            // iterate over attacker calls, let attacker process input
+            while self.limit > 0 {
+                let Some(mut call) = self.attacker.make_call(&mut self.attstate)
+                    else { break };
+                self.limit -= 1;
+                let (ret, gas, out) = create_evm_impl::<DB, true>(data, self).call(&mut call);
+                self.attacker.take_return(&mut self.attstate, ret, gas, out);
+            }
+            // decide whether a call should fail
+            let ok = self.attacker.check(&mut self.attstate);
+            let ok = if ok { InstructionResult::Continue } else { InstructionResult::Revert };
+            (ok, Gas::new(0), Bytes::default())
+        }
+        else {
+            // default: do nothing
             (InstructionResult::Continue, Gas::new(0), Bytes::default())
         }
     }
     fn call_end(
-        &mut self, _data: &mut EVMData<'_,DB>, inputs: &CallInputs, 
-        remaining_gas: Gas, ret: InstructionResult, out: Bytes
+        &mut self,
+        _data: &mut EVMData<'_, DB>,
+        inputs: &CallInputs,
+        remaining_gas: Gas,
+        ret: InstructionResult,
+        out: Bytes,
     ) -> (InstructionResult, Gas, Bytes) {
-        let contract = inputs.contract;
-        let bytes = &inputs.input;
-        let funct: u32 = u32::from_be_bytes(bytes.get(..4)
-            .map(|x| x.try_into().expect("length don't match"))
-            .unwrap_or([0u8; 4]));
-        if self.defender.contains_key(&(contract, funct)) { self.dfdstate.pop(); }
-        if inputs.context.caller == self.attacker.0 {
-            self.attstate = bitwig::evaluate(out.as_bits(), self.attstate.clone(), &self.attacker.2);
+        if self.accounts.0.contains(&inputs.contract) {
+            self.defstate.pop();
         }
         (ret, remaining_gas, out)
     }
